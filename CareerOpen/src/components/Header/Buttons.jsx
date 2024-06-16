d3ac29da59e24905977e4bb6c6f13d7ea66008f2
Kiwi <chiekechidinma7@gmail.com>
2024-06-16 02:23:57 +0100

    responsiveness

--- CareerOpen/src/components/Header/Buttons.jsx
+++ CareerOpen/src/components/Header/Buttons.jsx
@@ -1,11 +1,12 @@
 import { Link } from "react-router-dom"
+import './Buttons.css'
 
 const Buttons = () => {
   return (
  
-<div className=" flex">
+<div className="flex">
 
-    <div>
+    <div className="button">
      <Link to="/Signin" className='w-28 h-12 p-4 m-5 border-2 border-[#008552] bg-transparent mr-2 text-[#008552] rounded-md'>
         Sign In</Link>
      <Link to="/SignUp" className='w-28 h-12 p-4 m-5 text-white border-2 border-transparent bg-[#008552] rounded-md'
