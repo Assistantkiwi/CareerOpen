fd4b3787aeefc1b7e55b65d8873126a7f654f10e
Kiwi <chiekechidinma7@gmail.com>
2024-06-20 05:20:55 +0100

    additions

--- CareerOpen/src/components/Header/Buttons.jsx
+++ CareerOpen/src/components/Header/Buttons.jsx
@@ -7,13 +7,13 @@ const Buttons = () => {
       <div className="button">
         <Link
           to="../signin"
-          className="w-28 h-12 p-4 m-5 border-2 border-[#008552] bg-transparent mr-2 text-[#008552] rounded-md"
+          className="w-28 h-12 p-4 m-5 text-lg border-2 border-[#008552] bg-transparent mr-2 text-[#008552] rounded-md"
         >
           Sign In
         </Link>
         <Link
           to="../signup"
-          className="w-28 h-12 p-4 m-5 text-white border-2 border-transparent bg-[#008552] rounded-md"
+          className="w-28 h-12 p-4 m-5 text-lg text-white border-2 border-transparent bg-[#008552] rounded-md"
         >
           Sign Up
         </Link>
